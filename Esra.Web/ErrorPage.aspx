e34510994b8c4d4c3c214bea9d5cb0cbff4fa3b7
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:54:33 +0000

    Added Custom ErrorPage  (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1632. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5027

---
+++ Esra.Web/ErrorPage.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ErrorPage.aspx.cs" Inherits="CAESDO.Esra.Web.ErrorPage" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Error Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    Error: <asp:Label ID="lit_ErrorType" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Esra.Web/ErrorPage.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace CAESDO.Esra.Web
+{
+    public partial class ErrorPage : ApplicationPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                lit_ErrorType.Text = Server.HtmlDecode(Request.QueryString["reason"]);
+            }
+        }
+    }
+}
