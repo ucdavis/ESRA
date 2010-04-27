d1da7937b0558ddbf2138be37e7d51ce947ccc5c
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:58:23 +0000

    Revised to set and redirect URL to ErrorPage if user is not in ROLE_ADMIN. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1884. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5243

--- Esra.Web/UserAdminPage.aspx.cs
+++ Esra.Web/UserAdminPage.aspx.cs
@@ -18,7 +18,16 @@ namespace CAESDO.Esra.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                if (!IsAdminUser())
+                {
+                    string hl = "<br /><a href='Default.aspx'>Return to Home</a>";
+                    string redirectURL = "~/ErrorPage.aspx?reason=" + ERROR_MESSAGE_NOT_AUTHORIZED + hl;
+                    
+                    Response.Redirect(redirectURL);
+                }
+            }
         }
 
         protected void GViewUsers_SelectedIndexChanged(object sender, EventArgs e)
