2b67b6449ba9d43a0a157bf561562d043f62f4c2
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:55:58 +0000

    Added application title (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1729. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5123

--- Esra.Web/Esra.Master.designer.cs
+++ Esra.Web/Esra.Master.designer.cs
@@ -41,6 +41,15 @@ namespace CAESDO.Esra.Web {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// lblTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitle;
+        
+        /// <summary>
         /// lbl_Message control.
         /// </summary>
         /// <remarks>
