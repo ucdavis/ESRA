8f29d6aa43a864bdb63d1758ad73b5a068cf50c8
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:54:11 +0000

    Added SalaryScaleBLL and TitleBLL (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1606. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5001

---
+++ CAESDO.Esra.BLL/TitleBLL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using CAESDO.Esra.Core.DataInterfaces;
+using CAESDO.Esra.Core.Domain;
+using CAESDO.Esra.Data;
+
+namespace CAESDO.Esra.BLL
+{
+    public class TitleBLL : GenericBLL<Title, int>
+    {
+
+        public static Title GetByTitleCode(int id)
+        {
+            Title retval = null;
+
+            if (id > 0)
+            {
+
+                retval = GetByID(id);
+            }
+
+            return retval;
+        }
+    }
+}
