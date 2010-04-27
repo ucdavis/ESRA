1a1587d459d2c70b7d0ed2c3545c00143f214c41
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 15:02:53 +0000

    Update AssemblyVersion and AssemblyFileVersion to "2010.03.24.01" (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6537. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5496

--- Esra.Web/Properties/AssemblyInfo.cs
+++ Esra.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2009.01.21.01")]
-[assembly: AssemblyFileVersion("2009.01.21.01")]
+[assembly: AssemblyVersion("2010.03.24.01")]
+[assembly: AssemblyFileVersion("2010.03.24.01")]
