10e910d6e711eeed759d5f0003b16436a01a40d8
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 15:01:35 +0000

    Added an exposed entityset to the genericBLL. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4320. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5426

--- CAESDO.Esra.BLL/GenericBLL.cs
+++ CAESDO.Esra.BLL/GenericBLL.cs
@@ -1,12 +1,18 @@
 ﻿using System.ComponentModel;
 using CAESArch.BLL;
 using CAESDO.Esra.Core.DataInterfaces;
+using System.Linq;
 
 namespace CAESDO.Esra.BLL
 {
     [DataObject]
     public class GenericBLL<T, IdT> : GenericBLLBase<T,IdT>
     {
+        public static IQueryable<T> EntitySet
+        {
+            get { return Queryable; }
+        }
+
         public static IDaoFactory daoFactory
         {
             get
