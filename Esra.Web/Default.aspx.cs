e3d389b100a3a6caf75b985da79c9f7b58e870e1
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:55:22 +0000

    Revised ddlEmployee_SelectedIndexChanged to check if id is null or = 0 before executing EmployeeBLL.GetByID method. Revised btnSearch_Click to check if id is null or empty before executing logic to set TitleCode to selected Employee's TitleCode. Readded work-around code for refreshing page with update TimeInTile and YearsOfService.  (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1691. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5085

--- Esra.Web/Default.aspx.cs
+++ Esra.Web/Default.aspx.cs
@@ -42,7 +42,7 @@ namespace CAESDO.Esra.Web
                 {
                     ddlEmployee.SelectedValue = employeeID;
                 }
-                 
+                
             }
 
             /*
