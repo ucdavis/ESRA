d05d155fa7c4e0e0a944e8e5555bdc7d023106d1
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 15:01:38 +0000

    Checked in SQL script to add PPSCareerHireDateChecked and PPSApptHireDateChecked to ChangeTypes, Employee and SRAEmployee. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4328. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5429

---
+++ Esra.Database/Change Scripts/Update Employee - Add columns PPSCareerHireDateChecked and PPSApptHireDateChecked.sql
@@ -0,0 +1,18 @@
+﻿USE [Esra];
+GO
+
+ALTER TABLE dbo.Employee ADD "PPSCareerHireDateChecked" bit NULL;
+ALTER TABLE dbo.Employee ADD "PPSApptHireDateChecked" bit NULL;
+GO
+
+ALTER TABLE dbo.SRAEmployee ADD "PPSCareerHireDateChecked" bit NULL;
+ALTER TABLE dbo.SRAEmployee ADD "PPSApptHireDateChecked" bit NULL;
+GO
+
+SET IDENTITY_INSERT [dbo].ChangeType ON;
+INSERT INTO dbo.ChangeType ([ChangeTypeID], [Type], [ShortType], [Description]) VALUES (106, N'PPSCareerHireDateChecked', N'HireDateChecked', N'True to signify that a user has checked the employee''s career hire date against PPS.');
+INSERT INTO dbo.ChangeType ([ChangeTypeID], [Type], [ShortType], [Description]) VALUES (107, N'PPSApptHireDateChecked', N'ApptDateChecked', N'True to signify that a user has checked the employee''s appt. hire date against PPS.');
+SET IDENTITY_INSERT [dbo].[SelectionType] OFF;
+GO
+
+
