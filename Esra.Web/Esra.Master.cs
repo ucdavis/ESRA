4d183d392e4bec796515ac8761a3a416f371b2de
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:55:55 +0000

    Added new master page. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1726. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5120

---
+++ Esra.Web/Esra.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace CAESDO.Esra.Web
+{
+    public partial class Esra : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lbl_Version.Text = "Version : " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            if (!Page.IsPostBack)
+            {
+                if (Session["Message"] != null)
+                {
+                    this.lbl_Message.Text = Session["Message"].ToString();
+                    Session["Message"] = null;
+                }
+            }
+        }
+    }
+}
