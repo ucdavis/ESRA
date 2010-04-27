8fa0c2a64775078d8c891b60c246b929edfaf5d1
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:56:09 +0000

    Removed Linq "using" statements. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1743. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5137

--- Esra.Web/Default.aspx.cs
+++ Esra.Web/Default.aspx.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Data;
 using System.Configuration;
-using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
-using System.Xml.Linq;
 using CAESDO.Esra.BLL;
 using CAESDO.Esra.Core.Domain;
 using CAESDO.Esra.Data;
