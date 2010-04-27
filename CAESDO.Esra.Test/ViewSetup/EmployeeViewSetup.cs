11589d4dc3c25b32b9e4b82918ae9b4e9f4c4057
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 15:01:29 +0000

    Added in additional fields to the ucd employeed view. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4310. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5421

--- CAESDO.Esra.Test/ViewSetup/EmployeeViewSetup.cs
+++ CAESDO.Esra.Test/ViewSetup/EmployeeViewSetup.cs
@@ -1,5 +1,6 @@
 ﻿using CAESArch.Data.NHibernate;
 using System.Data.SqlClient;
+using System;
 
 namespace CAESDO.Esra.Test.ViewSetup
 {
@@ -13,16 +14,18 @@ namespace CAESDO.Esra.Test.ViewSetup
                 string employeeId = i.ToString();
                 employeeId = employeeId.PadLeft(9, '9');
 
+                string titleCode = "4722";
                 string dept = "030000";
                 
                 var cmdInsertUCDEmployee = NHibernateSessionManager.Instance.GetSession().Connection.CreateCommand();
                 cmdInsertUCDEmployee.CommandText = 
-                    string.Format("INSERT INTO UCDEmployee (PkEmployee, HomeDept) VALUES ('{0}', '{1}')", employeeId, dept);
+                    string.Format("INSERT INTO UCDEmployee (PkEmployee, EmployeeID, HomeDept, WorkDept, FirstName, LastName, TC, SalaryGrade, BargainingUnit)"
+                                + "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}')", employeeId + titleCode, employeeId, dept, dept, "First", "Last", titleCode, "1", "99");
 
                 cmdInsertUCDEmployee.ExecuteNonQuery();
 
                 var cmdInsertEmployee = NHibernateSessionManager.Instance.GetSession().Connection.CreateCommand();
-                cmdInsertEmployee.CommandText = string.Format("INSERT INTO Employee (PkEmployee) VALUES ('{0}')", employeeId);
+                cmdInsertEmployee.CommandText = string.Format("INSERT INTO Employee (PkEmployee) VALUES ('{0}')", employeeId + titleCode);
 
                 cmdInsertEmployee.ExecuteNonQuery();   
             }
