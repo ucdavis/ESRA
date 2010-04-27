7c9e626bc0849e49b524c4117584e040c9320481
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 15:01:43 +0000

    Added class ChangeTypeBLL.cs to project. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4341. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5434

---
+++ CAESDO.Esra.BLL/ChangeTypeBLL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using CAESArch.BLL;
+using CAESDO.Esra.Core.DataInterfaces;
+using CAESDO.Esra.Core.Domain;
+
+namespace CAESDO.Esra.BLL
+{
+    [DataObject]
+    public class ChangeTypeBLL : GenericBLL<ChangeType, int>
+    {
+    }
+}
