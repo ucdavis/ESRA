805bccf955ed1259683318927d26d5bba404e5a1
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:55:15 +0000

    Added new class as an example of how to make a composite key. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1683. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5077

---
+++ CAESDO.Esra.Core/Domain/UCDEmlpoyeeID.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAESDO.Esra.Core.Domain
+{
+
+    [Serializable]
+    public class UCDEmlpoyeeID
+    {
+        protected string _EmployeeID;
+        
+        public virtual string EmployeeID
+        {
+            get { return _EmployeeID; }
+            set { _EmployeeID = value; }
+        }
+
+        protected string _TC;
+
+        public virtual string TC
+        {
+            get { return _TC; }
+            set { _TC = value; }
+        }
+    
+        public override int GetHashCode()
+        {
+            return Convert.ToInt32(_EmployeeID) ^ Convert.ToInt32(_TC);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj)
+            {
+                return true;
+            }
+            UCDEmlpoyeeID key = obj as UCDEmlpoyeeID;
+            if (key == null)
+            {
+                return false;
+            }
+            if (this._EmployeeID != key._EmployeeID || this._TC != key._TC)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return _EmployeeID + _TC;
+        }
+
+        public UCDEmlpoyeeID() { }
+
+    }
+}
