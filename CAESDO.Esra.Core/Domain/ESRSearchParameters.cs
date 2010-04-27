fb2f24368885edb87f4325e6ef0c390b8ae33881
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:55:39 +0000

    Created new class to contain Employee Salary Report search parameters. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1710. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5104

---
+++ CAESDO.Esra.Core/Domain/ESRSearchParameters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CAESDO.Esra.Core.Domain;
+
+namespace CAESDO.Core.Domain
+{
+    public class ESRSearchParameters : DomainObject<ESRSearchParameters, int>
+    {
+        private List<Title> _SearchTitles;
+        // Contains a list of the Titles that were present in the Employee Salary Report search criteria.
+        public virtual List<Title> SearchTitles
+        {
+            get { return _SearchTitles; }
+            set { _SearchTitles = value; }
+        }
+
+        private List<Department> _SearchDepartments;
+        // Contains a list of the Departments that were present in the Employee Salary Report search criteria.
+        public virtual List<Department> SearchDepartments
+        {
+            get { return _SearchDepartments; }
+            set { _SearchDepartments = value; }
+        }
+
+        private Employee _SearchEmployee;
+        // Contains the Employee that was present in the Employee Salary Report search criteria.
+        public virtual Employee SearchEmployee
+        {
+            get { return _SearchEmployee; }
+            set { _SearchEmployee = value; }
+        }
+
+        private DateTime _ReportDate;
+
+        public virtual DateTime ReportDate
+        {
+            get { return _ReportDate; }
+            set { _ReportDate = value; }
+        }
+
+        public ESRSearchParameters()
+        {
+            _ReportDate = DateTime.Today;
+        }
+
+    }
+}
