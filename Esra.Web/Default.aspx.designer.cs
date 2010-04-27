b94886cd32655a8e0e781b2e5ca884fdbc85b10d
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:54:04 +0000

    Changed namespace to CAESDO.Esra.Web (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1600. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C4995

--- Esra.Web/Default.aspx.designer.cs
+++ Esra.Web/Default.aspx.designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CAESDO {
+namespace CAESDO.Esra.Web {
     
     
     public partial class _Default {
