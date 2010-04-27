5e6961583ecf1feb027a91b31400eabf78a9539c
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:56:03 +0000

    Created new BLL class for SelectionType (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1735. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5129

---
+++ CAESDO.Esra.BLL/SelectionTypeBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using CAESDO.Esra.Core.DataInterfaces;
+using CAESDO.Esra.Core.Domain;
+using CAESDO.Esra.Data;
+
+namespace CAESDO.Esra.BLL
+{
+    public class SelectionTypeBLL : GenericBLL<SelectionType, int>
+    {
+        public static SelectionType GetByType(string type)
+        {
+            if (String.IsNullOrEmpty(type) || type.Equals("0"))
+            {
+               type = SelectionType.NONE;
+            }
+
+            return GetByProperty("Type", type);
+        }
+    }
+}
