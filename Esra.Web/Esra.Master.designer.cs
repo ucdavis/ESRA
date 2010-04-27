db204d4d0239e543f17ba94dfcda2ff065dea3bd
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:57:51 +0000

    Added home button image and home image link.  (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1845. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5218

--- Esra.Web/Esra.Master.designer.cs
+++ Esra.Web/Esra.Master.designer.cs
@@ -86,6 +86,15 @@ namespace CAESDO.Esra.Web {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentBody;
         
         /// <summary>
+        /// imgBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton imgBtn;
+        
+        /// <summary>
         /// lbl_Version control.
         /// </summary>
         /// <remarks>
