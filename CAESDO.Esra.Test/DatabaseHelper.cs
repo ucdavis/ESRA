23050fed15eced5d1d85c8d7c7d385b8b6d73b85
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 15:01:27 +0000

    Added the main database test files to esra. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4306. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5418

---
+++ CAESDO.Esra.Test/DatabaseHelper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CAESDO.Esra.Test
+{
+    public static class DatabaseHelper
+    {
+        public static void LoadData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CAESDO.Esra.Test/EntityIdSetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace CAESDO.Esra.Test
+{
+    public class EntityIdSetter
+    {
+        /// <summary>
+        /// Uses reflection to set the Id of an object
+        /// </summary>
+        public static void SetIdOf<IdT>(object entity, IdT id)
+        {
+            // Set the data property reflectively
+            PropertyInfo idProperty = entity.GetType().GetProperty("ID",
+                                                                   BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (idProperty == null) throw new ArgumentException("idProperty could not be found");
+
+            idProperty.SetValue(entity, id, null);
+        }
+    }
+}
