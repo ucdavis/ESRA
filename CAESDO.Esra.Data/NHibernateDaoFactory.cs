d9a6decdc6af2f54e7533b8602b64e6ac5a3d5f8
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:55:32 +0000

    Added methods public IList<Employee> GetEmployees(string propertyName, bool ascending, string titleCode, string pkEmployee, string[] departmentIDs) and public IList<Employee> GetEmployees(string propertyName, bool ascending, string[] titleCodes, string pkEmployee, string[] departmentIDs) (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1703. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5097

--- CAESDO.Esra.Data/NHibernateDaoFactory.cs
+++ CAESDO.Esra.Data/NHibernateDaoFactory.cs
@@ -99,6 +99,108 @@ namespace CAESDO.Esra.Data
                 }
                 return retval;
             }
+
+            public IList<Employee> GetEmployees(string propertyName, bool ascending, string titleCode, string pkEmployee, string[] departmentIDs)
+            {
+                IList<Employee> retval = null;
+
+                if ((String.IsNullOrEmpty(titleCode) || titleCode.Equals("0")) &&
+                    ((String.IsNullOrEmpty(pkEmployee)) || pkEmployee.Equals("0")) &&
+                    ((departmentIDs == null) || (departmentIDs.Length == 0) || (departmentIDs[0].Equals("0"))))
+                {
+                    retval = GetAll(propertyName, ascending);
+                }
+                else
+                {
+                    ICriteria criteria = NHibernateSessionManager.Instance.GetSession().CreateCriteria(typeof(Employee));
+                    Conjunction conjunction = Expression.Conjunction();
+
+                    if (String.IsNullOrEmpty(titleCode) == false && titleCode.Equals("0") == false)
+                    {
+                        criteria.CreateAlias("Title", "Title");
+                        conjunction.Add(Expression.Eq("Title.TitleCode", titleCode));
+                    }
+                    if (String.IsNullOrEmpty(pkEmployee) == false && pkEmployee.Equals("0") == false)
+                    {
+                        conjunction.Add(Expression.Eq("id", pkEmployee));
+                    }
+                    if ((departmentIDs != null) && (departmentIDs.Length > 0) && (departmentIDs[0].Equals("0") == false))
+                    {
+                        criteria.CreateAlias("HomeDepartment", "Department");
+                        conjunction.Add(Expression.In("Department.id", departmentIDs));
+                    }
+                    criteria.Add(conjunction);
+
+                    if (propertyName.Equals("HomeDepartment"))
+                    {
+                        criteria.CreateAlias("HomeDepartment", "HomeDepartment")
+                        .AddOrder((ascending ? Order.Asc("HomeDepartment.Name") : Order.Desc("HomeDepartment.Name")))
+                        .AddOrder(Order.Asc("FullName"));
+                    }
+                    else
+                    {
+                        criteria.AddOrder((ascending ? Order.Asc(propertyName) : Order.Desc(propertyName)));
+                        if (propertyName.Equals("FullName") == false)
+                        {
+                            criteria.AddOrder(Order.Asc("FullName"));
+                        }
+                    }
+
+                    retval = criteria.List<Employee>();
+                }
+                return retval;
+            }
+
+            public IList<Employee> GetEmployees(string propertyName, bool ascending, string[] titleCodes, string pkEmployee, string[] departmentIDs)
+            {
+                IList<Employee> retval = null;
+
+                if ((titleCodes == null) || (titleCodes.Length == 0) || (titleCodes[0].Equals("0")) &&
+                    ((String.IsNullOrEmpty(pkEmployee)) || pkEmployee.Equals("0")) &&
+                    ((departmentIDs == null) || (departmentIDs.Length == 0) || (departmentIDs[0].Equals("0"))))
+                {
+                    retval = GetAll(propertyName, ascending);
+                }
+                else
+                {
+                    ICriteria criteria = NHibernateSessionManager.Instance.GetSession().CreateCriteria(typeof(Employee));
+                    Conjunction conjunction = Expression.Conjunction();
+
+                    if ((titleCodes != null) && (titleCodes.Length > 0) && (titleCodes[0].Equals("0") == false))
+                    {
+                        criteria.CreateAlias("Title", "Title");
+                        conjunction.Add(Expression.In("Title.TitleCode", titleCodes));
+                    }
+                    if (String.IsNullOrEmpty(pkEmployee) == false && pkEmployee.Equals("0") == false)
+                    {
+                        conjunction.Add(Expression.Eq("id", pkEmployee));
+                    }
+                    if ((departmentIDs != null) && (departmentIDs.Length > 0) && (departmentIDs[0].Equals("0") == false))
+                    {
+                        criteria.CreateAlias("HomeDepartment", "Department");
+                        conjunction.Add(Expression.In("Department.id", departmentIDs));
+                    }
+                    criteria.Add(conjunction);
+
+                    if (propertyName.Equals("HomeDepartment") && ((departmentIDs == null) || (departmentIDs.Length == 0) || (departmentIDs[0].Equals("0"))))
+                    {
+                        criteria.CreateAlias("HomeDepartment", "HomeDepartment")
+                        .AddOrder((ascending ? Order.Asc("HomeDepartment.Name") : Order.Desc("HomeDepartment.Name")))
+                        .AddOrder(Order.Asc("FullName"));
+                    }
+                    else
+                    {
+                        criteria.AddOrder((ascending ? Order.Asc(propertyName) : Order.Desc(propertyName)));
+                        if (propertyName.Equals("FullName") == false)
+                        {
+                            criteria.AddOrder(Order.Asc("FullName"));
+                        }
+                    }
+
+                    retval = criteria.List<Employee>();
+                }
+                return retval;
+            }
         }
 
         #endregion
