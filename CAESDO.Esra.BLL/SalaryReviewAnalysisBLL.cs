13e3fd228fd201a94b45da1484241d1d420895a4
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:54:45 +0000

    Added new SalaryReviewAnalysisBLL (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1644. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5039

---
+++ CAESDO.Esra.BLL/SalaryReviewAnalysisBLL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using CAESDO.Esra.Core.DataInterfaces;
+using CAESDO.Esra.Core.Domain;
+using CAESDO.Esra.Data;
+
+namespace CAESDO.Esra.BLL
+{
+    public class SalaryReviewAnalysisBLL : GenericBLL<SalaryReviewAnalysis, int>
+    {
+       
+    }
+}
