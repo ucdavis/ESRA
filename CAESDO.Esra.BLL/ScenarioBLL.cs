22029d5f87528c3ab84252c4280bd4b736614ebf
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:57:02 +0000

    Added ScenarioBLL.cs to project (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1777. )(Edited by AESDEAN\sutaylor)
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5171

---
+++ CAESDO.Esra.BLL/ScenarioBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using CAESDO.Esra.Core.DataInterfaces;
+using CAESDO.Esra.Core.Domain;
+using CAESDO.Esra.Data;
+
+namespace CAESDO.Esra.BLL
+{
+    public class ScenarioBLL : GenericBLL<Scenario, int>
+    {
+       public static IList<Scenario> GetBySalaryReviewAnalysisID(int sarID)
+       {
+           IList<Scenario> retval = null;
+
+           Scenario scenario = new Scenario() { SalaryReviewAnalysisID = sarID };
+           retval = GetByInclusionExample(scenario, "ScenarioNumber", true, "SalaryReviewAnalysisID");
+
+           return retval;
+       }
+    }
+}
