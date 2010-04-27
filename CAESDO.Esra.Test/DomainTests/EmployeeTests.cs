b690cfc058c5d47d5932b28f5118addaef622c61
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 15:01:30 +0000

    Can now insert dates into the base ucd employee view. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4313. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5423

--- CAESDO.Esra.Test/DomainTests/EmployeeTests.cs
+++ CAESDO.Esra.Test/DomainTests/EmployeeTests.cs
@@ -8,22 +8,6 @@ namespace CAESDO.Esra.Test.DomainTests
     [TestClass]
     public class EmployeeTests : DatabaseTestBase
     {
-        [TestMethod]
-        public void Tester()
-        {
-            var emps = EmployeeBLL.GetAll();
-            var employee = emps[0];
 
-            employee.CareerHireDate = DateTime.Now;
-            employee.CareerDateHasBeenAdjusted = true;
-
-            using (var ts = new TransactionScope())
-            {
-                EmployeeBLL.EnsurePersistent(employee);
-
-                ts.CommitTransaction();
-            }
-
-        }
     }
 }
--- CAESDO.Esra.Test/ViewSetup/EmployeeViewSetup.cs
+++ CAESDO.Esra.Test/ViewSetup/EmployeeViewSetup.cs
@@ -18,10 +18,17 @@ namespace CAESDO.Esra.Test.ViewSetup
                 
                 var cmdInsertUCDEmployee = NHibernateSessionManager.Instance.GetSession().Connection.CreateCommand();
                 cmdInsertUCDEmployee.CommandText = 
-                    string.Format("INSERT INTO UCDEmployee (PkEmployee, EmployeeID, HomeDept, WorkDept, FirstName, LastName, TC, SalaryGrade, BargainingUnit)"
-                                + "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}')", employeeId + titleCode, employeeId, dept, dept, "First", "Last", titleCode, "1", "99");
+                    string.Format("INSERT INTO UCDEmployee (PkEmployee, EmployeeID, HomeDept, WorkDept, FirstName, LastName, TC, SalaryGrade, BargainingUnit, HireDate)"
+                                + "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}')", employeeId + titleCode, employeeId, dept, dept, "First", "Last", titleCode, "1", "99", "2009-07-07");
 
                 cmdInsertUCDEmployee.ExecuteNonQuery();
+
+                /*
+                cmdInsertUCDEmployee.CommandText =
+                    "UPDATE UCDEmployee SET BeginDate = '2009-07-07' WHERE PkEmployee = '9999999914722'";
+
+                cmdInsertUCDEmployee.ExecuteNonQuery();
+                */
 
                 var cmdInsertEmployee = NHibernateSessionManager.Instance.GetSession().Connection.CreateCommand();
                 cmdInsertEmployee.CommandText = string.Format("INSERT INTO Employee (PkEmployee) VALUES ('{0}')", employeeId + titleCode);
