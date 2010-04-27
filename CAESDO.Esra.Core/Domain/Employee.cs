d0883465186a63b467956aa1b89370273daca8b3
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:54:58 +0000

    Removed logic from database fields, and added "class only" methods so as to avoid automatic database update on select.  (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1663. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5057

--- CAESDO.Esra.Core/Domain/Employee.cs
+++ CAESDO.Esra.Core/Domain/Employee.cs
@@ -7,14 +7,79 @@ namespace CAESDO.Esra.Core.Domain
 {
     public class Employee : UCDEmployee, IComparable<Employee>
     {
+        private bool _CareerDateHasBeenAdjusted;
+        // Not a database field.
+        public virtual bool CareerDateHasBeenAdjusted
+        {
+            get
+            {
+                if (_CareerHireDate == null)
+                    return false;
+                else if (_CareerHireDate == _HireDate)
+                    return false;
+                else
+                    return true;
+            }
+            set { _CareerDateHasBeenAdjusted = value; }
+        }
+
+        private DateTime _AdjustedCareerHireDate;
+        // Not a database field.
+        public virtual DateTime AdjustedCareerHireDate
+        {
+            get
+            {
+                if (_CareerHireDate != null)
+                {
+                    return (DateTime)_CareerHireDate;
+                }
+                else
+                {
+                    return _HireDate;
+                }
+            }
+            set { _AdjustedCareerHireDate = value; }
+        }
+
+        private bool _ApptDateHasBeenAdjusted;
+        // Not a database field.
+        public virtual bool ApptDateHasBeenAdjusted
+        {
+            get
+            {
+                if (_ApptHireDate == null)
+                    return false;
+                else if (_ApptHireDate == _BeginDate)
+                    return false;
+                else
+                    return true;
+            }
+            set { _ApptDateHasBeenAdjusted = value; }
+        }
+
+        private DateTime _AdjustedApptHireDate;
+        // Not a database field.
+        public virtual DateTime AdjustedApptHireDate
+        {
+            get
+            {
+                if (_ApptHireDate != null)
+                {
+                    return (DateTime)_ApptHireDate;
+                }
+                else
+                {
+                    return _BeginDate;
+                }
+            }
+            set { _AdjustedApptHireDate = value; }
+        }
+
         private bool _DatesHaveBeenAdjusted;
 
         public virtual bool DatesHaveBeenAdjusted
         {
-            get
-            {
-                return _DatesHaveBeenAdjusted ;
-            }
+            get { return _DatesHaveBeenAdjusted ; }
             set { _DatesHaveBeenAdjusted = value; }
         }
 
@@ -22,82 +87,30 @@ namespace CAESDO.Esra.Core.Domain
 
         public virtual DateTime? CareerHireDate
         {
-            get 
-            {
-                //if (_CareerHireDate != null)
-                //{
-                //    return (DateTime)_CareerHireDate;
-                //}
-                //else
-                //{
-                //    return _HireDate;
-                //}
-                return _CareerHireDate;
-            }
-            set
-            {
-                _CareerHireDate = value;
-
-                if (value != null)
-                {
-                    DatesHaveBeenAdjusted = true;
-                }
-            }
+            get { return _CareerHireDate; }
+            set { _CareerHireDate = value; }
         }
 
         private DateTime? _ApptHireDate;
 
         public virtual DateTime? ApptHireDate
         {
-            get
-            {
-                //if (_ApptHireDate != null)
-                //{
-                //    return (DateTime)_ApptHireDate;
-                //}
-                //else
-                //{
-                //    return _BeginDate;
-                //}
-                return _ApptHireDate;
-            }
-            set 
-            {
-                _ApptHireDate = value;
-
-                if (value != null)
-                {
-                    DatesHaveBeenAdjusted = true;
-                }
-            }
+            get { return _ApptHireDate; }
+            set { _ApptHireDate = value; }
         }
 
         private double? _YearsOfService;
 
         public virtual double? YearsOfService
         {
-            get
-            {
-                //return (DateTime.Today - (DateTime)CareerHireDate).TotalDays / 365.25;
-                return _YearsOfService;
-            }
-            set
-            {
-                _YearsOfService = value;
-            }
+            get { return _YearsOfService; }
+            set { _YearsOfService = value; }
         }
         private double? _TimeInTitle;
         public virtual double? TimeInTitle
         {
-            get
-            {
-                //return (DateTime.Today - (DateTime)ApptHireDate).TotalDays / 365.25;
-                return _TimeInTitle;
-            }
-            set
-            {
-                _TimeInTitle = value;
-            }
+            get { return _TimeInTitle; }
+            set { _TimeInTitle = value; }
         }
 
         private string _DepartmentComments;
