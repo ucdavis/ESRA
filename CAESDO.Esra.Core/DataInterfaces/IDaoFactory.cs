ead9b3da6d0585dbc3f16316cd75e01ad7c6148b
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:55:32 +0000

    Added interface def'n  IList<Employee> GetEmployees(string propertyName, bool ascending, string[] titleCodes, string pkEmployee, string[] departmentIDs); (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1702. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5096

--- CAESDO.Esra.Core/DataInterfaces/IDaoFactory.cs
+++ CAESDO.Esra.Core/DataInterfaces/IDaoFactory.cs
@@ -23,6 +23,7 @@ namespace CAESDO.Esra.Core.DataInterfaces
         IList<Employee> GetByTitleCode(string titleCode, string propertyName, bool ascending);
         IList<Employee> GetEmployees(string propertyName, bool ascending, string titleCode, string pkEmployee, string departmentID);
         IList<Employee> GetEmployees(string propertyName, bool ascending, string titleCode, string pkEmployee, string[] departmentIDs);
+        IList<Employee> GetEmployees(string propertyName, bool ascending, string[] titleCodes, string pkEmployee, string[] departmentIDs);
     }
 
     #endregion
