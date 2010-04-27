640306d28499add22158ffa3747d0eeced35ba55
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 15:02:57 +0000

    Added Insert into ChangeTypes.sql  (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6542. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5501

---
+++ Esra.Database/Create Scripts/Insert Data/Insert into ChangeTypes.sql
@@ -0,0 +1,16 @@
+﻿ SET IDENTITY_INSERT [ESRA].[dbo].[ChangeType] ON
+
+INSERT INTO [ESRA].[dbo].[ChangeType]
+           ([ChangeTypeID],
+            [Type]
+           ,[ShortType]
+           ,[Description]) VALUES
+('101'	,'AdjustedCareerHireDate',	'HireDate',	'Adjusted University Career Hire Date.'),
+('102'	,'AdjustedApptHireDate',	'ApptDate',	'Adjusted Appointment Begin Date in Title.'),
+('103'	,'ExperienceBeginDate',	'ExperienceDate',	'Adjusted Experience Begin Date.'),
+('104'	,'DepartmentComments',	'DeptComments',	'Generic comments regarding employee entered by Employee''s department.'),
+('105'	,'DeansOfficeComments',	'DeansComments',	'Generic comments regarding employee entered by Dean''s Office.'),
+('106'	,'PPSCareerHireDateChecked',	'HireDateChecked',	'True to signify that a user has checked the employee''s career hire date against PPS.'),
+('107'	,'PPSApptHireDateChecked',	'ApptDateChecked',	'True to signify that a user has checked the employee''s appt. hire date against PPS.')
+
+SET IDENTITY_INSERT [ESRA].[dbo].[ChangeType] OFF
