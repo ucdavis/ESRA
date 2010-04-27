8c982ce23bcdcd179061ba7608e13a6d8bfadedf
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:59:06 +0000

    Added new BLL class: SRAEmployeeBLL (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1966. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/ESRA/Trunk/Esra;C5290

---
+++ CAESDO.Esra.BLL/SRAEmployeeBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using CAESDO.Esra.Core.DataInterfaces;
+using CAESDO.Esra.Core.Domain;
+using CAESDO.Esra.Data;
+
+namespace CAESDO.Esra.BLL
+{
+    public class SRAEmployeeBLL : GenericBLL<SRAEmployee, string>
+    {
+        public static void UpdateRecord(SRAEmployee record)
+        {
+            using (var ts = new TransactionScope())
+            {
+                EnsurePersistent(ref record);
+                ts.CommittTransaction();
+            }
+        }        
+    }
+}
