ab5aa044c514e1259042bf4e13f9821d42e80bc7
Scott Kirkland <srkirkland@gmail.com>
2011-06-27 08:48:51 -0700

    Added an account controller and account pages so everyone logging in needs to do CAS authentication

---
+++ Esra.Web/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using UCDArch.Web.Authentication;
+
+namespace Esra.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the Account class
+    /// </summary>
+    public class AccountController : Controller
+    {
+        public string Message
+        {
+            set { TempData["Message"] = value; }
+        }
+        public ActionResult LogOn(string returnUrl)
+        {
+            string resultUrl = CASHelper.Login(); //Do the CAS Login
+
+            if (resultUrl != null)
+            {
+                return Redirect(resultUrl);
+            }
+
+            TempData["URL"] = returnUrl;
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("https://cas.ucdavis.edu/cas/logout");
+        }
+
+        public RedirectToRouteResult Emulate(string id /* Login ID*/)
+        {
+            if (User.IsInRole("EmulationUser"))
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    Message = "Emulating " + id;
+                    FormsAuthentication.RedirectFromLoginPage(id, false);
+                }
+                else
+                {
+                    Message = "Login ID not provided.  Use /Emulate/login";
+                }
+            }
+            else
+            {
+                Message = "You do not have permission to perform this action";
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Esra.Web/Controllers/ApplicationController.cs
+++ Esra.Web/Controllers/ApplicationController.cs
@@ -1,8 +1,10 @@
 using UCDArch.Web.Attributes;
 using UCDArch.Web.Controller;
+using System.Web.Mvc;
 
 namespace Esra.Web.Controllers
 {
     [Version(MajorVersion = 2)]
+    [Authorize]
     public class ApplicationController : SuperController { }
 }
