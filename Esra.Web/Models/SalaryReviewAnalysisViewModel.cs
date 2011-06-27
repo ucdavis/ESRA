ec27508b90af17f2b9340f452945802aa324ce97
taylorkj <kentaylor@ucdavis.edu>
2011-06-27 15:21:24 -0700

    Revised color and bold setting code to allow both in the same column.

--- Esra.Web/Models/SalaryReviewAnalysisViewModel.cs
+++ Esra.Web/Models/SalaryReviewAnalysisViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Esra.Core.Domain;
 using UCDArch.Core.PersistanceSupport;
 using UCDArch.Core.Utils;
@@ -10,8 +11,35 @@ namespace Esra.Web.Models
     /// </summary>
     public class SalaryReviewAnalysisViewModel
     {
-        public IList<Employee> Employees { get; set; }
+        public String HiddenUserId { get; set; }
 
+        public bool HiddenIsDepartmentUser { get; set; }
+
+        // The SalaryReviewAnalysis creation date sought for
+        // formerly populated from tbCreationDate
+        public String CreationDateString { get; set; }
+
+        // This is a list of all possible users that can be "seen" based on the logged-in user's unit(s) and role Name
+        // Used to populate the "Created By" drop-down search list.
+        // formerly ddlCreatedBy: DataTextField: FullName; DataValueField: LoginID
+        public IList<User> FilteredUsers { get; set; }
+
+        // List of all possible employees that can be "seen" by the logged-in user
+        // Used to populate the "Employee" drop-down search list.
+        // formerly ddlEmployee: DataTextField: FullName; DataValueField: ID
+        public IList<Employee> FilteredEmployees { get; set; }
+
+        // List of all EmployeeSalaryReviewAnalysis that can be "seen" based on the logged in user's unit(s) and role name
+        // Used to populate the Reference Number drop-down search list.
+        // formerly ddlReferenceNumber: DataTextField: ReferenceNumber; DavaValueField: ID
+        public IList<SalaryReviewAnalysis> FilteredSalaryReviewAnalysis { get; set; }
+
+        //List of all the matching SalaryReviewAnalysis meeting the search criteria
+        // Used to populate the SalaryReviewAnalysis results listing
+        // formerly gvSalaryReviewAnalysis
+        public IList<SalaryReviewAnalysis> SalaryReviewAnalysisResults { get; set; }
+
+        // This is the particular salary review analysis sought, once selected from list
         public SalaryReviewAnalysis SalaryReviewAnalysis { get; set; }
 
         public static SalaryReviewAnalysisViewModel Create(IRepository repository)
